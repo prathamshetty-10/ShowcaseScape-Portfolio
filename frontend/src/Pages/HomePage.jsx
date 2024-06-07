8a250dc875c3ea4b05f27bca742c8f7bebcf0d29
Pratham shetty <prathamshetty484@gmail.com>
2024-06-07 13:10:52 +0530

    new commit

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -55,7 +55,7 @@ function HomePage(){
         Welcome to ShowcaseScape! Explore my curated portfolio, showcasing the pinnacle of my creative journey. <span className="hidden lg:block">Discover stunning designs and innovative projects, each crafted with passion and precision. Dive in and get inspired by a landscape of talent and imagination.</span>
         </div>
         <button  className=" mx-[25%] w-[50%] lg:w-[50%] mt-6 lg:mt-8 bg-yellow-600 hover:bg-yellow-400 transition-all ease-in-out duration-300 text-black font-semibold hover:text-blue-950 rounded-lg py-3 text-lg cursor-pointer  mb-[50px] lg:mb-[0px] mr-[30px] lg:mr-[0px] hover:scale-105">
-        <a href="https://github.com/prathamshetty-10/portfolio-REACT" target="_blank" className="flex justify-center items-center gap-3 text-xl">
+        <a href="https://github.com/prathamshetty-10/ShowcaseScape-Portfolio" target="_blank" className="flex justify-center items-center gap-3 text-xl">
         <LuGithub size={32}/>
         <span className="font-bold text-2xl">Explore</span>
                 
